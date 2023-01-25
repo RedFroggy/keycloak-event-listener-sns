112d503bfaccf1e55541b809f7d7ae3255e9e225
Claire Labbé <claire.labbe@redfroggy.fr>
2023-01-25 17:15:47 +0100

    chore: add transaction in provider class and tests

--- src/main/java/fr/redfroggy/keycloak/SnsEventListenerProvider.java
+++ src/main/java/fr/redfroggy/keycloak/SnsEventListenerProvider.java
@@ -2,26 +2,41 @@ package fr.redfroggy.keycloak;
 
 import org.keycloak.events.Event;
 import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventListenerTransaction;
 import org.keycloak.events.admin.AdminEvent;
+import org.keycloak.models.KeycloakTransactionManager;
 public class SnsEventListenerProvider implements EventListenerProvider {
 
     private final SnsEventPublisher snsEventPublisher;
 
-    public SnsEventListenerProvider(SnsEventPublisher snsEventPublisher) {
+    private final EventListenerTransaction transaction = new EventListenerTransaction(this::sendAdminEvent, this::sendEvent);
+
+    public SnsEventListenerProvider(SnsEventPublisher snsEventPublisher, KeycloakTransactionManager transactionManager) {
         this.snsEventPublisher = snsEventPublisher;
+        transactionManager.enlistAfterCompletion(transaction);
     }
 
     @Override
     public void onEvent(Event event) {
-        snsEventPublisher.sendEvent(new SnsEvent(event));
+        transaction.addEvent(event);
+       
     }
 
     @Override
     public void onEvent(AdminEvent event, boolean includeRepresentation) {
-        snsEventPublisher.sendAdminEvent(new SnsAdminEvent(event));       
+        transaction.addAdminEvent(event, includeRepresentation);      
     }
 
     @Override
     public void close() {
     }
+
+    private void sendEvent(Event event){
+        snsEventPublisher.sendEvent(new SnsEvent(event));
+    }
+
+    private void sendAdminEvent(AdminEvent adminEvent, boolean includeRepresentation){
+        snsEventPublisher.sendAdminEvent(new SnsAdminEvent(adminEvent)); 
+    }
+
 }
